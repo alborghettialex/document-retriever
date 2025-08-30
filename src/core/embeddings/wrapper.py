01b3c1ea257d069655004c55f78ab6a318b6e7b0
alborghettialex1998@gmail.com <alborghettialex1998@gmail.com>
2025-08-30 18:20:44 +0200

    :memo: add docstring to `ChonkieEmbeddingWrapper`
    Also fixed a bug (         `emb.vector` should not be a list)

--- src/core/embeddings/wrapper.py
+++ src/core/embeddings/wrapper.py
@@ -3,6 +3,9 @@ from src.core.embeddings.algorithms import HFDenseTextEmbeddingsGenerator
 from transformers import AutoTokenizer
 
 class ChonkieEmbeddingWrapper(BaseEmbeddings):
+    """
+    A wrapper class for generating embeddings for Chonkie's semantic chunker.
+    """
     def __init__(self, embedder: HFDenseTextEmbeddingsGenerator):
         super().__init__()
         self.embedder = embedder
@@ -12,7 +15,7 @@ class ChonkieEmbeddingWrapper(BaseEmbeddings):
         return self.embedder.dim
 
     def embed(self, text: str) -> list[float]:
-        emb = self.embedder.get_embedding(text, return_tensor=False)
+        emb = self.embedder.get_embedding(text, return_numpy=True)
         return emb.vector
 
     def embed_documents(self, texts: list[str]) -> list[list[float]]:
